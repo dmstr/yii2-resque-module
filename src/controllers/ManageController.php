139e826cd2d76e3eb96bbfeedf07f16f7fac7557
Elias Luhr <e.luhr@herzogkommunikation.de>
2024-04-02 15:42:26 +0200

    removed unused imports, use check if post has loaded model

--- src/controllers/ManageController.php
+++ src/controllers/ManageController.php
@@ -8,37 +8,28 @@
 
 namespace hrzg\resque\controllers;
 
-
-use bedezign\yii2\audit\Audit;
-use hrzg\resque\components\Job;
 use hrzg\resque\components\QueueCommand;
 use hrzg\resque\models\QueueForm;
-use yii\filters\AccessControl;
-use yii\helpers\Json;
-use yii\helpers\VarDumper;
+use Yii;
 use yii\web\Controller;
 
 class ManageController extends Controller
 {
     public function actionIndex()
     {
-        $model = new QueueForm;
+        $model = new QueueForm();
 
-        if (\Yii::$app->request->post('QueueForm')) {
-            $model->load($_POST);
+        if ($model->load($this->request->post())) {
 
-            \Yii::$app->queue->push(new QueueCommand([
+            Yii::$app->queue->push(new QueueCommand([
                 'command' => $model['command'],
-                'sessionId' => \Yii::$app->session->id
+                'sessionId' => Yii::$app->getSession()->getId()
             ]));
         }
 
-        return $this->render(
-            'index',
-            [
-                'model' => $model,
-            ]
-        );
+        return $this->render('index', [
+            'model' => $model
+        ]);
 
     }
 }
